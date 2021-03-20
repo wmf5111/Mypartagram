97a586779ddb9accfe04ab8e36f6f9c29add256f
Weijun Fan <fwrrong@163.com>
2021-03-20 17:55:49 +0800

    Initial Commit

---
+++ Mypartagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mypartagram
+//
+//  Created by 范为君 on 2021/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
